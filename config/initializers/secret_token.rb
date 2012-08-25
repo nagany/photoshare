ac97b42ed878cdab66962f3b869b1f5e7ac373a9
nagany <nagany0209@gmail.com>
2012-08-24 20:32:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photoshare::Application.config.secret_token = '5c7bd1edc216d9194c9f5eb62715a5abb9277eef1bba61d2a0d2f100252791ae5b1111d3b3620309c7d6aee12c5396d3cb799255e1b721759f51c18666d2336c'
